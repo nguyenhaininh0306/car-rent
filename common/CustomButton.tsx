5025f941ce2debfae31a9c732a891104fc7383e9
HaiNinh0306 <72057074+nguyenhaininh0306@users.noreply.github.com>
2024-06-10 15:35:56 +0700

    create section, background in homepage

---
+++ common/CustomButton.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { CustomButtonProps } from '@/types'
+import React from 'react'
+
+type Props = {}
+
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+}: CustomButtonProps) => {
+  return (
+    <button
+      disabled={false}
+      type={'button'}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className="flex-1 ">{title}</span>
+    </button>
+  )
+}
+
+export default CustomButton
