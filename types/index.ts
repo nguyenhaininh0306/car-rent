ae1298244e24837d221ac5605cd88ed4b3a509ce
HaiNinh0306 <72057074+nguyenhaininh0306@users.noreply.github.com>
2024-06-11 16:52:22 +0700

    updated code

--- types/index.ts
+++ types/index.ts
@@ -6,7 +6,7 @@ export interface CustomButtonProps {
   handleClick?: MouseEventHandler<HTMLButtonElement>
   btnType?: 'button' | 'submit'
   textStyles?: string
-  rightIcon: string
+  rightIcon?: string
   isDisabled?: boolean
 }
 
